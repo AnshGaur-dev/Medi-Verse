0902331a9da03552e7d0ab39ad83e34f4aebe9dd
AnshGaur-dev <anshgaur914@gmail.com>
2024-04-04 01:11:11 +0530

    Home page

--- app/src/main/java/com/example/medi_verse/Student/Screens/Home.kt
+++ app/src/main/java/com/example/medi_verse/Student/Screens/Home.kt
@@ -7,11 +7,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -22,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -76,11 +80,21 @@ fun Home() {
                         }
                     )
                 }
-            ) { Column(modifier = Modifier.padding(top=80.dp)) {
+            ) {
+                Column(modifier = Modifier.padding(top=80.dp)) {
                 Text(text = "Current Updates",color = Color.Black,
                     fontSize = 22.sp,
-                    fontFamily = FontFamily.Serif,fontWeight = FontWeight.W300, modifier = Modifier.padding(start = 17.dp))
-            }}
+                    fontFamily = FontFamily.Serif,fontWeight = FontWeight.W200, modifier = Modifier.padding(start = 17.dp))
+                Card (
+                    modifier= Modifier.size(height = 150.dp, width = 210.dp).padding(top=10.dp), shape = RoundedCornerShape(12.dp)
+
+                ){
+
+                }
+            }
+
+            }
+
 
         }
     }
